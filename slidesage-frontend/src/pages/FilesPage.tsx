873b87d0880792298514e990572174349c688da5
Anwar Reghai <reganwar36@gmail.com>
2025-09-14 15:54:55 -0400

    added filespage component

---
+++ slidesage-frontend/src/pages/FilesPage.tsx
@@ -0,0 +1,8 @@
+export default function FilesPage() {
+  return (
+    <div className="py-8">
+      <h1 className="text-2xl font-bold mb-4">My Files</h1>
+      <p className="text-gray-600">Files list placeholder</p>
+    </div>
+  );
+}
