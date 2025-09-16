60fed3264a8b328a0e89c9bc524c191ca89bd3d8
Anwar Reghai <reganwar36@gmail.com>
2025-09-16 01:56:25 -0400

    modified app/tsx

--- slidesage-frontend/src/App.tsx
+++ slidesage-frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Layout from './components/Layout';
+import ProtectedRoute from './components/ProtectedRoute';
 import HomePage from './pages/HomePage';
 import LoginPage from './pages/LoginPage';
 import FilesPage from './pages/FilesPage';
@@ -13,9 +14,21 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<LoginPage />} />
-          <Route path="/files" element={<FilesPage />} />
-          <Route path="/files/:id" element={<FileDetailPage />} />
-          <Route path="/settings" element={<SettingsPage />} />
+          <Route path="/files" element={
+            <ProtectedRoute>
+              <FilesPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/files/:id" element={
+            <ProtectedRoute>
+              <FileDetailPage />
+            </ProtectedRoute>
+          } />
+          <Route path="/settings" element={
+            <ProtectedRoute>
+              <SettingsPage />
+            </ProtectedRoute>
+          } />
         </Routes>
       </Layout>
     </Router>
