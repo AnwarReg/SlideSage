1cb7960fe965b45c2470c1a54842434ca684498d
Anwar Reghai <reganwar36@gmail.com>
2025-10-02 23:15:04 -0400

    modified repo

--- slidesage-backend/slidesage-backend/src/main/java/com/slidesage/slidesage_backend/files/FileRepository.java
+++ slidesage-backend/slidesage-backend/src/main/java/com/slidesage/slidesage_backend/files/FileRepository.java
@@ -2,9 +2,12 @@ package com.slidesage.slidesage_backend.files;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface FileRepository extends JpaRepository<FileEntity, UUID> {
+
+    Optional<FileDetailProjection> findByIdAndUserId(UUID id, UUID userId);
 
     // Return only the projection, not the whole entity
     List<FileItemProjection> findAllByUserIdOrderByUpdatedAtDesc(UUID userId);
